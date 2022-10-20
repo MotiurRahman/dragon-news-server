2c01bdfe25ad162dc3153f3c74f1cfdb589a8499
RRO (RSL-Md. Motiur Rahman) <RRO@rahimafrooz.com>
2022-10-20 17:37:55 +0600

    update code

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const categories = require("./Data/categories.json");
+const news = require("./Data/news.json");
 app.use(cors());
 
 app.get("/", (req, res) => {
@@ -11,6 +12,27 @@ app.get("/", (req, res) => {
 
 app.get("/news-categories", (req, res) => {
   res.send(categories);
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((news) => news._id == id);
+  res.send(selectedNews);
+});
+
+app.get("/categories/:id", (req, res) => {
+  const id = req.params.id;
+
+  if (id == 8) {
+    res.send(news);
+  } else {
+    const selectedCategory = news.filter((news) => news.category_id == id);
+    res.send(selectedCategory);
+  }
+});
+
+app.get("/news", (req, res) => {
+  res.send(news);
 });
 
 app.listen(port, () => {
